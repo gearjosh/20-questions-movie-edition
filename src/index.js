2a7aa9ebb05c0fa908d3bff7836bd81f7e2d97b6
Student <example@example.com>
2018-10-11 10:45:42 -0700

    separate logic

--- src/index.js
+++ src/index.js
@@ -6,25 +6,6 @@ import {processData} from './process-data.js';
 
 $(document).ready(function() {
 
-  let promise = new Promise(function(resolve, reject) {
-    let request = new XMLHttpRequest();
-    let url = movieList;
-    request.onload = function() {
-      if (this.status === 200) {
-        resolve(request.response);
-      } else {
-        reject(Error(request.statusText));
-      }
-    };
-    request.open("GET", url, true);
-    request.send();
-  });
 
-  promise.then(function(response) {
-    let body = processData(response);
-    console.log(body);
-  }, function(error){
-    console.log(error);
-  });
 
 });
