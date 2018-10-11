8455b43056f2e65c794c49bae2ae75500bc94efe
Student <example@example.com>
2018-10-11 09:33:49 -0700

    add test

--- spec/movie-spec.js
+++ spec/movie-spec.js
@@ -1,7 +1,17 @@
-// it should return a random movie name
+import Movie from './../src/movie.js'
 
-// it should return a random movie object from our OMDB API
+describe('Movie', function() {
+  // it should return a random movie name
+  it('should return a random movie name from movie-list.csv', function() {
+    
+    const actual = ;
+    const control = ;
+    expect(actual).toEqual(control);
+  };
+  // it should return a random movie object from our OMDB API
 
-// it should recognize the year keyword in user input
+  // it should recognize the year keyword in user input
 
-// if user asks about year output is a string of four numbers
+  // if user asks about year output is a string of four numbers
+
+});
